538479489178acac3a2d8e1af412b4653f65d75c
deepstar <jadeharu@daum.net>
2018-11-11 14:47:28 +0900

    remove webstom DXweb_bak directory

--- DXweb_bak/DXweb/views.py
+++
@@ -1,7 +0,0 @@
-#from django.shortcuts import render #원래있던것인데 책엔 없길레 일단 주석 처리함.181016
-
-# Create your views here.
-from django.views.generic.base import TemplateView
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
